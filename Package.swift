d550818070f2c5eafc2954cdda95042f7c3dfc0f
AliOSStemp <gaoguangzhi.ggz@alibaba-inc.com>
2026-07-27 15:11:47 +0800

    AliyunOSSiOS 2.11.3: dynamic framework to fix ObjC category stripping

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ let package = Package(
     platforms: [.iOS(.v12)],
     products: [.library(name: "AliyunOSSiOS", targets: ["AliyunOSSiOS"])],
     targets: [
-        .binaryTarget(name: "AliyunOSSiOS", url: "https://alivc-demo-cms.alicdn.com/versionProduct/sourceCode/upload/2.0.1/iOS/AliyunOSSiOS.xcframework.zip", checksum: "85a6aec8097bd1a90cab14b730cf7fc3deb360a064289e245381fb58c8a2410f")
+        // 动态 framework (mh_dylib): 修复静态库导致的 OC 分类被裁剪、
+        // 运行期 unrecognized selector(oss_setObject:forKey:) 崩溃。客户零配置。
+        .binaryTarget(name: "AliyunOSSiOS", url: "https://alivc-demo-cms.alicdn.com/versionProduct/sourceCode/upload/2.0.1/iOS/AliyunOSSiOS-2.11.3-dynamic.xcframework.zip", checksum: "9c2c03687b701505784e63b0107f14cb3f40d7629f21e54cd8dc69a6b2082b67")
     ]
 )
